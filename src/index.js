80d25a1c2e6c05574b8467fb773728c4769f485d
KwangJin <skrhkdwls77@gmail.com>
2019-09-02 19:25:46 +0900

    수정 및 삭제

--- src/index.js
+++ src/index.js
@@ -5,31 +5,47 @@ let users = [];
 let user = null;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000);
 
 app.get("/users", (req, res) => {
-    res.send("user "+user.name+" get");
+    if(user) {
+        res.send("user " + user.name +" get");
+    } else{
+        res.send("user id " + req.params.id+" 가 존재하지 않습니다.");    
+    }
 });
 
 app.get("/users/:id", (req, res) => {
-    if(user.id == req.params.id){
-    res.send("user"+user.name+" get");
-}
-    res.send("user id"+req.params.id+"가 존재하지 않습니다.");
+    if(user && user.id == req.params.id) {
+        res.send("user " + user.name +" get");
+    } else {
+        res.send("user id " + req.params.id+" 가 존재하지 않습니다.");
+    }
+
 });
 
 app.post("/users", (req, res) => {
     user = req.body;
-    res.send("user add" +user.name);
+    console.log(req.body);
+    res.send("user 추가 " + user.name);
 });
 
-app.put("/users:id", (req, res) => {
-    user = req.body;
-        res.send("user name "+user.name);
+app.put("/users/:id", (req, res) => {
+    if(user && user.id == req.params.id) {
+        user.name = req.body.name; 
+        res.send("user name" + user.name + "으로 변경");
+    } else {
+        res.send("user id " + req.params.id + "가 존재하지 않습니다.")
+    }
 });
 
 app.delete("/users/:id", (req, res) => {
-    res.send("user "+req.params.id+" delete");
+    if(user && user.id == req.params.id) {
+        user = null;
+        res.send("user id " + req.params.id + " 삭제");
+    } else {
+        res.send("user id " + req.params.id + " 가 존재하지 않습니다.");
+    }
 });
